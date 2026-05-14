3b7b050c5a41efccdca81d6f529adaf27ad5b66f
seweex <seweex@gmail.com>
2026-05-14 18:40:52 +0300

    refactor and adapt for tests

--- shader_link/main.py
+++ shader_link/main.py
@@ -6,8 +6,46 @@ from pathlib import Path
 from shader_link.logger import Logger
 from shader_link.executor import Executor, FilesToCompile, Settings
 
+def parse_args ():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-r", "--replace",
+        required=False,
+        default=False,
+        help="recompile forced with no skips",
+        action="store_true")
+
+    parser.add_argument(
+        "-i", "--input",
+        required=True,
+        help="input shader directory or file",
+        type=str)
+
+    parser.add_argument(
+        "-o", "--output",
+        required=False,
+        default="./out/",
+        help="output shader directory or file",
+        type=str)
+
+    return parser.parse_args ()
+
+def run_compilation (logger : Logger,
+                     settings : Settings,
+                     target : FilesToCompile):
+    try:
+        executor = Executor()
+        executor.compile (settings, target, logger)
+    except KeyboardInterrupt:
+        logger.report_interrupted()
+    except Exception as error:
+        logger.report_fatal (str(error))
+    else:
+        logger.report_done ()
+
 def main():
-    logger = Logger()
+    logger = Logger ()
 
     try:
         parser = argparse.ArgumentParser()
@@ -34,21 +72,15 @@ def main():
 
         args = parser.parse_args()
 
-        input_path = Path (str (args.input))
-        output_path = Path (str (args.output))
-
-        executor = Executor ()
         settings = Settings (args.replace)
-        target = FilesToCompile (input_path, output_path)
+        target = FilesToCompile (Path (args.input), Path (args.output))
 
-        executor.compile (settings, target, logger)
+        run_compilation (logger, settings, target)
 
     except KeyboardInterrupt:
         logger.report_interrupted()
     except Exception as error:
         logger.report_fatal (str(error))
-    else:
-        logger.report_done ()
 
 if __name__ == "__main__":
     main()
