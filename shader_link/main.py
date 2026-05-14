8c29279e05a1eaea85ec5c90a6e019dcda907d4f
seweex <seweex@gmail.com>
2026-05-14 18:42:23 +0300

    removed doubled code

--- shader_link/main.py
+++ shader_link/main.py
@@ -48,29 +48,7 @@ def main():
     logger = Logger ()
 
     try:
-        parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            "-r", "--replace",
-            required=False,
-            default=False,
-            help="recompile forced with no skips",
-            action="store_true")
-
-        parser.add_argument(
-            "-i", "--input",
-            required=True,
-            help="input shader directory or file",
-            type=str)
-
-        parser.add_argument(
-            "-o", "--output",
-            required=False,
-            default="./out/",
-            help="output shader directory or file",
-            type=str)
-
-        args = parser.parse_args()
+        args = parse_args ()
 
         settings = Settings (args.replace)
         target = FilesToCompile (Path (args.input), Path (args.output))
